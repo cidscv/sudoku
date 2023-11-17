d572304d48cea7d41bbe38afe80ba9fb48819109
Owen Reid <owen.reid2@ontariotechu.net>
2023-11-17 16:40:20 -0500

    still have work to do, need to figure out logic for createRandomBoard

--- game.py
+++ game.py
@@ -1,7 +1,29 @@
+import random
 from board import Board
 
-def main():
+def createRandomBoard():
+    board = Board()
 
+    check = 0
+
+    while check != 13:
+        x_rng = random.randint(1, 9)
+        y_rng = random.randint(1, 9)
+        num_rng = random.randint(1, 9)
+
+        board.insertToBoard(x_rng, y_rng, str(num_rng))
+
+        if board.checkIllegalNum() == False:
+            board.insertToBoard(x_rng, y_rng, 'X')
+        else:
+            check += 1
+
+    board.printBoard()
+    print(board.solve())
+    board.printBoard()
+
+def main():
+    """
     board = Board()
     board.insertToBoard(4, 1, '8')
     board.insertToBoard(5, 1, '3')
@@ -45,6 +67,9 @@ def main():
 
     board.solve()
     board.printBoard()
+    """
+
+    createRandomBoard()
 
 if __name__ == '__main__':
     main()
